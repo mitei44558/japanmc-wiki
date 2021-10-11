00e2e0c3bda592de6fec167d88c47ee1eb34a5b9
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-10-11 23:23:54 +0900

    Init commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,24 @@
+/**
+ * @type {import('vitepress/types').UserConfig}
+ */
+module.exports = {
+  title: 'JapanMC Wiki (非公式)',
+  description: 'Japan MCの非公式Wikiです',
+  lang: 'ja-jp',
+  themeConfig: {
+    repo: 'tsutoringo/japanmc-wiki',
+    docsDir: 'docs',
+    editLinks: true,
+    editLinkText: 'このページをGitHubで編集する',
+    lastUpdated: '最終更新日',
+    nav: []
+  },
+  markdown: {
+    config: (md) => {
+      md.use(require('markdown-it-multimd-table'), {
+        rowspan: true,
+      });
+      md.use(require('markdown-it-scrolltable'));
+    }
+  }
+}
---
+++ docs/index.md
@@ -0,0 +1,17 @@
+---
+title: トップページ
+---
+
+# JapanMC wikiへようこそ！🎉
+このWikiは有志により作成されており非公式です！
+一応、サーバー主さんからは許可をとってあります
+
+もしこのWikiへの要望があればtsutoringo#8666までDMを送るか
+GitHubでpull requestを送信してください！
+
+## 目次
+### ガイド
+- [MCMMO(スキル)](/guides/mcmmo.md)
+- [Towny(街・国家管理)](/guides/towny.md)
+- [Shop(アイテム売買)](/guides/shop.md)
+- [ユーザーショップ(ユーザー間アイテム売買)](/guides/user-shop.md)
